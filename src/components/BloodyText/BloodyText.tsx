d862af391c09a81b2cf9bf13aaf352984e8907fb
Trlaic <igortrlajic@gmail.com>
2024-02-05 23:43:43 +0100

    x04: Fix the reload/save issue

--- src/components/BloodyText/BloodyText.tsx
+++ src/components/BloodyText/BloodyText.tsx
@@ -1,14 +1,14 @@
-import React, { ReactElement, useEffect, useRef } from "react"
+import React, { ReactElement, useEffect, useRef, useState } from "react"
 
 import styles from './BloodyText.module.scss'
-import { cssNameDependingOnElementCount } from "../functions/countHtmlElements"
+import { cssNameDependingOnElementCount } from "../../functions/countHtmlElements"
 
 interface ComponentProps {
     text: string
 }
 
 const BloodyText: React.FC<ComponentProps> = ({text}) => {
-    
+    const [loading, setLoading] = useState(true)
     const el = useRef<ReactElement[]>([])
 
     useEffect(() => {
@@ -19,8 +19,14 @@ const BloodyText: React.FC<ComponentProps> = ({text}) => {
             arr.push(<span key={i} className={`${styles.letter} ${styles[letter]}`}>{'\u00a0'}</span>)
         }
         el.current = arr
+        setLoading(false)
     }, [text.length])
 
+    if(loading) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
     return (
         <h2 className={styles.heading}>
             {text}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
-import BloodyText from '../components/BloodyText'
+import BloodyText from '../components/BloodyText/BloodyText'
 import styles from './HomePage.module.scss'
 
 function HomePage() {
     return (
         <div className={styles.container}>
-            <BloodyText text='memory' />
-            <BloodyText text='game' />
+            <div className={styles.letters}>
+                <BloodyText text='memory' />
+                <BloodyText text='game' />
+            </div>
         </div>
     ) 
 }
