5bd76bc4e9cbe64af610a047139040c4ccc00a0c
Trlaic <igortrlajic@gmail.com>
2024-02-14 12:16:44 +0100

    x10: Move Audio component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import TheAudio from "./components/Audio/TheAudio"
 
 function App() {
   return(
     <BrowserRouter>
+      <TheAudio />    
       <Routes>
         <Route path="/" element={<HomePage />}/>
       </Routes>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import TheAudio from '../components/Audio/TheAudio'
 import BloodyText from '../components/BloodyText/BloodyText'
 import CardsHomePage from '../components/CardsHomePage'
 import styles from './HomePage.module.scss'
@@ -6,7 +5,6 @@ import styles from './HomePage.module.scss'
 function HomePage() {
     return (
         <div className={styles.container}>
-            <TheAudio />
             <div className={styles.letters}>
                 <BloodyText text='memory' />
                 <BloodyText text='game' />
